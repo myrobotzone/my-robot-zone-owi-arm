e894c0f49bf7d495b0db588d4b450d4fcb360fa7
myrobotzone <jonas.hodel@gmail.com>
2016-03-13 20:15:23 +0100

    Initial comit

---
+++ my-robot-zone-owi-arm/my-robot-zone-owi-arm/Program.cs
@@ -0,0 +1,93 @@
+﻿using owi_arm_dotnet;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_robot_zone_owi_arm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to start the server!");
+
+            Console.ReadKey();
+            Console.WriteLine();
+
+            var appServer = new AppServer();
+
+            //Setup the appServer
+            if (!appServer.Setup(5000)) //Setup with listening port
+            {
+                Console.WriteLine("Failed to setup!");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine();
+
+            //Try to start the appServer
+            if (!appServer.Start())
+            {
+                Console.WriteLine("Failed to start!");
+                Console.ReadKey();
+                return;
+            }
+
+            IOwiArm arm = new OwiArm();
+            IOwiCommand command = new OwiCommand();
+
+            try
+            {
+                arm.Connect();
+            }
+            catch(Exception e)
+            {
+                appServer.Stop();
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+                return;
+            }
+
+            var newSessionHandler = new SessionHandler<AppSession>(appServer_NewSessionConnected);
+            appServer.NewSessionConnected += newSessionHandler;
+
+            var newRequestReceived = new RequestHandler<AppSession, StringRequestInfo>((session, requestInfo) => appServer_NewRequestReceived(session, requestInfo, arm, command));
+            appServer.NewRequestReceived += newRequestReceived;
+
+            Console.WriteLine("The server started successfully, press key 'q' to stop it!");
+
+            while (Console.ReadKey().KeyChar != 'q')
+            {
+                Console.WriteLine();
+                continue;
+            }
+
+            //Stop the appServer
+            appServer.Stop();
+            arm.SendCommand(command.StopAllMovements().LedOff());
+            arm.Disconnect();
+
+            appServer.NewSessionConnected -= newSessionHandler;
+            appServer.NewRequestReceived -= newRequestReceived;
+
+            Console.WriteLine("The server was stopped!");
+            Console.ReadKey();
+        }
+
+        private static void appServer_NewSessionConnected(AppSession session)
+        {
+            Console.WriteLine(string.Format("Session connected: {0}", session.RemoteEndPoint.Address));
+        }
+
+        private static void appServer_NewRequestReceived(AppSession session, StringRequestInfo requestInfo, IOwiArm arm, IOwiCommand command)
+        {
+            Console.WriteLine(string.Format("Received message {0}", requestInfo.Body));
+            arm.SendCommand(command.LedOn());
+        }
+    }
+}
---
+++ my-robot-zone-owi-arm/my-robot-zone-owi-arm/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("my-robot-zone-owi-arm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("my-robot-zone-owi-arm")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cd1c3988-f6ac-4898-a2d3-1e9e268cf82d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
